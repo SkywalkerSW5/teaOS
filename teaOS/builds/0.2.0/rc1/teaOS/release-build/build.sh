97babec91b2be7548e95d71a0013f637f15afa3c
SkywalkerSW5 <skywalkersw5@hotmail.com>
2021-01-15 15:05:13 +0000

    Adding on to PKGBUILD

--- teaOS/builds/0.2.0/rc1/teaOS/release-build/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-echo "Building teaOS"
-genisoimage -J -allow-lowercase -R -iso-level 2 -o teaOS-$(date +%Y.%m.%d).$(uname -m).iso ./
-echo "Finished building"
--- teaOS/builds/0.2.0/rc1/teaOS/release-build/utils/license/README.md
+++
@@ -1,3 +0,0 @@
-# A collection of licenses for teaOS and its programs
-## BSD 3 Clause - ddw
-## MIT License - sys-info
